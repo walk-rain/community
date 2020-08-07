ee561508d00c42dbbebd85036fb513c9ddabeada
walk-rain <walk_rain@126.com>
2020-08-07 16:52:08 +0800

    登录逻辑

--- src/main/java/com/dhcc/community/controller/AuthrizeController.java
+++ src/main/java/com/dhcc/community/controller/AuthrizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthrizeController {
 
@@ -24,7 +26,8 @@ public class AuthrizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -33,7 +36,13 @@ public class AuthrizeController {
         accessTokenDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = gitHubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user!=null) {
+            // 登录成功，保存用户信息到Session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            // 登录失败
+            return "redirect:/";
+        }
     }
 }
