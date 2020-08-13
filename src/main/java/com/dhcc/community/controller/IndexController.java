a3b9856f341918983ca2ae8da5306828c86e4f59
walk-rain <walk_rain@126.com>
2020-08-13 23:48:25 +0800

    add check cookies

--- src/main/java/com/dhcc/community/controller/IndexController.java
+++ src/main/java/com/dhcc/community/controller/IndexController.java
@@ -1,13 +1,30 @@
 package com.dhcc.community.controller;
 
+import com.dhcc.community.mapper.UserMapper;
+import com.dhcc.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals("token")) {
+                User user = userMapper.findByToken(cookie.getValue());
+                request.getSession().setAttribute("user",user);
+                break;
+            }
+        }
         return "index";
     }
 }
